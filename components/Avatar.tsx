7e29b259af4e829dfa9a01724223aeee25f36cbd
Eric Cecchi <eric.cecchi@sproutsocial.com>
2020-10-28 17:20:22 -0500

    add next-on-netlify

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
-
 const Avatar = ({src = '/me.jpg', width = '100px', height = '100px', ...rest}) => {
-    return <Image src={src} width={width} height={height} {...rest} className='block rounded-full' />
+    return <img src={src} width={width} height={height} {...rest} className='block rounded-full' />
 }
 
 export default Avatar;
